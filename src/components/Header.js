68a324d0ce214ad54c5efb4583b790c0f8047633
suyeon <suyeon@vivans.net>
2020-11-09 17:23:57 +0900

    헤더에 뒤로가기 버튼 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { toggleAddMode } from '../actions/index';
 
 function Header(props) {
   
+  const dispatch = useDispatch();
   const selected = useSelector(state => state.selected);
   const addInput = useSelector(state => state.addInput);
   const [ title, setTitle ] = useState(false);
 
   useEffect(() => {
-    if(selected && addInput) {
+    if(addInput) {
       setTitle(true);
     } else {
       props.history.push('/')
@@ -19,11 +21,17 @@ function Header(props) {
   console.log(title);
   //props.location.pathname === "/todolist"
 
+  const onClickGoBack = () => {
+    props.history.push('/');
+    dispatch(toggleAddMode());
+  }
+
   return (
     <div>
       {title && selected ?
         <div className="selected">
-          <span className="selected-day">{selected.day.toUpperCase()},</span>
+          <i className="icon-chevron-left" onClick={onClickGoBack}/>
+          <span className="selected-day">{selected.day.toUpperCase()}, </span>
           <span className="selected-date">{selected.date}</span>
         </div> :
         <h1>TODO TODO</h1> 
